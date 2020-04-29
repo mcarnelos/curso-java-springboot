e948f6591da42e28271b3fece5b6d13df1865344
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-28 22:28:01 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/murilocarnelos/cursomc/services/CategoriaService.java
+++ src/main/java/com/murilocarnelos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.murilocarnelos.cursomc.domain.Categoria;
+import com.murilocarnelos.cursomc.dto.CategoriaDTO;
 import com.murilocarnelos.cursomc.repositories.CategoriaRepository;
 import com.murilocarnelos.cursomc.services.exceptions.DataIntegrityException;
 import com.murilocarnelos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,9 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 	
 }
 	
