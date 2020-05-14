b6c945bd6858a27e21b7c5f792e61aae3f77ddf1
Murilo Carnelos <muriloc.c@hotmail.com>
2020-05-14 20:20:48 -0300

    Email HTML

--- src/main/java/com/murilocarnelos/cursomc/services/EmailService.java
+++ src/main/java/com/murilocarnelos/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.murilocarnelos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.murilocarnelos.cursomc.domain.Pedido;
@@ -9,5 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	//email html
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
