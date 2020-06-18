8bdb66843d7a2c01b2d985c1ed626b5b9b76992c
Murilo Carnelos <muriloc.c@hotmail.com>
2020-06-17 22:30:10 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/murilocarnelos/cursomc/config/SecurityConfig.java
+++ src/main/java/com/murilocarnelos/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.murilocarnelos.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -41,9 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
+			"/categorias/**"
 	};		
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**"
+			
+	};	
 			
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
@@ -52,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		}
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()//permite que seja feito post no vetor public_,matchers_post
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()//permite tudo do PUBLIC_MATCHERS
 			.anyRequest().authenticated();//qualquer outra requisição solicita autenticação
